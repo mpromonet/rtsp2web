e4e78bb24f491f044a74651d3ae477521e2b0418
Michel Promonet <michel.promonet@free.fr>
2024-08-03 21:25:20 +0200

    move worker in a webcomponent

--- www/video-ws/worker.js
+++ www/video-ws/worker.js
@@ -8,9 +8,14 @@
 import { MediaStream } from './mediastream.js';
 
 self.onmessage = (event) => {
-    const offscreenCanvas = event.data.canvas;
-    console.log('worker received canvas', offscreenCanvas);
-
-    const mediaStream = new MediaStream(offscreenCanvas, null, (loaded) => postMessage({type: 'loaded', loaded}));
-    mediaStream.connect(event.data.url);
+    if (event.data.canvas) {
+        const offscreenCanvas = event.data.canvas;
+        console.log('worker received canvas', offscreenCanvas);
+    
+        self.mediaStream = new MediaStream(offscreenCanvas, null, (loaded) => postMessage({type: 'loaded', loaded}));
+    
+    }
+    if (event.data.url && self.mediaStream) {
+        self.mediaStream.connect(event.data.url);
+    }
 };
